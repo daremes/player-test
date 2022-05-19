43fbdf595c60f3409e7fc0d0ae65ab27a33e595b
Martin Semerad <martin.sem84@gmail.com>
2022-05-19 06:22:52 +0200

    styling

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { getHash } from "../utils/getHash";
 import IframePlayer from "../components/IframePlayer";
 import { createUseStyles } from "react-jss";
+import { FaPlay } from "react-icons/fa";
 
 const CHANNELS = [
   "CH_1",
@@ -51,149 +52,215 @@ const useStyles = createUseStyles({
   idInput: {
     margin: 2,
   },
+  categoryWrapper: {
+    margin: [8, 0],
+  },
+  categoryTitle: {
+    textTransform: "uppercase",
+    fontSize: 14,
+    fontWeight: "bold",
+    color: "#444",
+  },
+  exampleWrapper: { display: "flex", margin: [8, 0] },
+  exampleDescription: { fontSize: 14, padding: [0, 8] },
+  exampleTitle: {},
+  exampleId: { color: "#666" },
+  playButton: {
+    border: "#000",
+    borderRadius: 4,
+    background: "#666",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 40,
+    cursor: "pointer",
+  },
+  link: {
+    fontSize: 14,
+    color: "#111",
+    wordWrap: "break-word",
+  },
 });
 
 const EXAMPLES = [
   {
-    title: "Rapstory 1/10 - reklamy i pro Safari",
+    title: "Rapstory 1/10",
+    type: "Reklamy i pro Safari",
     idec: "220562280010001",
     videoTitle: "Rapstory 1/10",
     showId: "1234",
   },
   {
-    title: "Bonus - Novinarky",
+    title: "Novinarky",
+    type: "Bonus",
     bonus: "42128",
     videoTitle: "Novinářky",
     showId: "1234",
   },
   {
-    title: "Bonus - pouze audio",
+    title: "Pouze audio",
+    type: "Audio only",
     bonus: "32433",
     videoTitle: "1968: Dnes před 50 lety",
     showId: "12171864678",
   },
   {
-    title: "Princip slasti 1/10 - dabing, puvodni zneni, AD, titulky",
+    title: "Princip slasti 1/10",
+    type: "Dabing, puvodni zneni, AD, titulky",
     idec: "417233100051001",
     videoTitle: "Princip slasti 1/10",
     showId: "1234",
   },
   {
-    title: "Princip slasti - 18+ labeling soucasti playlistu",
+    title: "Princip slasti",
+    type: "18+ labeling soucasti playlistu",
     idec: "219254002211001",
     videoTitle: "Princip slasti 1/10",
     showId: "1234",
   },
   {
-    title: "Mimořádné pořady ČT24 1. březen - indexy",
+    title: "Mimořádné pořady ČT24 1. březen",
+    type: "Indexy",
     idec: "222411033230301",
     videoTitle: "Mimořádné pořady ČT24 1. březen",
     showId: "1234",
   },
   {
-    title: "Světy Jindřicha Chalupeckého - playback samostatneho indexu",
+    title: "Světy Jindřicha Chalupeckého",
+    type: "Jeden index",
     // idec: "2222041120000120318",
     index: "900837",
     videoTitle: "Světy Jindřicha Chalupeckého",
     showId: "1234",
   },
   {
-    title: "Vrazedne stiny ep 2 - labeling soucasti vastu",
+    title: "Vrazedne stiny ep 2",
+    type: "Labeling soucasti vastu",
     idec: "220512120060002",
     videoTitle: "Vražedné stíny 2/10",
     showId: "1234",
   },
   {
-    title: "Vicenasobny playlist/indexy - Studio kamarad",
+    title: "Studio kamarad",
+    type: "Vicenasobny playlist/indexy",
     idec: "222553110020020",
     videoTitle: "Nevim",
     showId: "1234",
   },
   {
-    title: "Vicenasobny playlist/indexy - Wifina",
+    title: "Wifina",
+    type: "Vicenasobny playlist/indexy",
     idec: "222553112050019",
     videoTitle: "Nevim",
     showId: "1234",
   },
   {
-    title: "Vicenasobny playlist/indexy",
+    title: "Neco",
+    type: "Vicenasobny playlist/indexy",
     idec: "322291310020006",
     videoTitle: "Nevim",
     showId: "1234",
   },
   {
-    title: "Cyklotoulky - AD",
+    title: "Cyklotoulky",
+    type: "Audio description",
     idec: "221471292050042",
     videoTitle: "Cyklotoulky Opava",
     showId: "1234",
   },
   {
-    title: "Angličtina s Hurvínkem - AD",
+    title: "Angličtina s Hurvínkem",
+    type: "Audio description",
     idec: "214542157910035",
     videoTitle: "Angličtina s Hurvínkem",
     showId: "1234",
   },
   {
-    title: "Klobouček / Dan Bárta trio - pouze jedna kvalita",
+    title: "Klobouček / Dan Bárta trio",
+    type: "Pouze jedna kvalita",
     idec: "21154215446",
     videoTitle: "Klobouček / Dan Bárta trio",
     showId: "1234",
   },
   {
-    title: "Země orlů - omezeny set kvalit",
+    title: "Země orlů",
+    type: "Omezeny set kvalit",
     idec: "26153232260",
     videoTitle: "Země orlů",
     showId: "1234",
   },
   {
-    title: "Písničky pro pamětníky - bez fullHD",
+    title: "Písničky pro pamětníky",
+    type: "Bez FullHD",
     idec: "27832024069",
     videoTitle: "Písničky pro pamětníky",
     showId: "1234",
   },
   {
-    title: "Pocasi - video pod 5min",
+    title: "Pocasi",
+    type: "Video pod 5min",
     idec: "206411000400124",
     videoTitle: "Pocasi",
     showId: "1234",
   },
   {
-    title: "O pohár prezidenta ČTS - video nad 5h",
+    title: "O pohár prezidenta ČTS",
+    type: "Video nad 5h",
     idec: "220471291352003",
     videoTitle: "O pohár prezidenta ČTS",
     showId: "1234",
   },
   {
-    title: "Území strachu 4:3",
+    title: "Území strachu",
+    type: "4:3 aspect ratio",
     idec: "28631033712",
     videoTitle: "Území strachu",
     showId: "1234",
   },
   {
-    title: "Kosovo - krev není voda - non-provys",
+    title: "Kosovo - krev není voda",
+    type: "Non-provys",
     idec: "21225100058",
     videoTitle: "Kosovo - krev není voda",
     showId: "1234",
   },
   {
-    title: "168 hodin - skryte titulky",
+    title: "168 hodiny",
+    type: "Closed captions",
     idec: "222452801100410",
     videoTitle: "10. duben",
     showId: "10117034229",
   },
   {
-    title: "168 hodin - skryte titulky",
+    title: "168 hodin",
+    type: "Closed captions",
     idec: "222452801100403",
     videoTitle: "3. duben",
     showId: "10117034229",
   },
   {
-    title: "168 hodin - skryte titulky",
+    title: "168 hodin",
+    type: "Closed captions",
     idec: "222452801100327",
     videoTitle: "27. březen",
     showId: "10117034229",
   },
 ];
+
+const getCategorized = () => {
+  const categories: string[] = [];
+  const categorized: any[] = [];
+  EXAMPLES.forEach((example) => {
+    if (!categories.includes(example.type)) {
+      categories.push(example.type);
+      const sameType = EXAMPLES.filter((ex) => ex.type === example.type);
+      categorized.push(sameType);
+    }
+  });
+  return categorized;
+};
+
+const categorized = getCategorized();
 
 const ENVS = [
   "https://player.ceskatelevize.cz/",
@@ -462,38 +529,53 @@ export default function Home() {
           </div>
         </div>
         {hasId && (
-          <div>
-            {ENVS.map((env) => (
-              <div key={env} style={{ margin: "8px 0" }}>
-                <a
-                  href={`${env}?${queryString}`}
-                  target="_blank"
-                  rel="noreferrer"
-                >{`${env}?${queryString}`}</a>
-              </div>
-            ))}
+          <div style={{ margin: "8px 0" }}>
+            <a
+              className={classes.link}
+              href={`${ENVS[envIndex]}?${queryString}`}
+              target="_blank"
+              rel="noreferrer"
+            >{`${ENVS[envIndex]}?${queryString}`}</a>
           </div>
         )}
         <div style={{ marginTop: 32 }}>
-          <b>Příklady:</b>
-          {EXAMPLES.map((ex) => (
+          {categorized.map((category) => (
             <div
-              key={ex.idec || ex.bonus || ex.index}
-              style={{ margin: "6px 0" }}
+              className={classes.categoryWrapper}
+              key={JSON.stringify(category)}
             >
-              <div>{ex.title}</div>
-              <button
-                onClick={() => {
-                  setId({
-                    ...DEFAULT_OPTIONS,
-                    ...(ex.idec ? { idec: ex.idec } : {}),
-                    ...(ex.bonus ? { bonus: ex.bonus } : {}),
-                    ...(ex.index ? { index: ex.index } : {}),
-                  });
-                }}
-              >
-                {ex.index || ex.idec || ex.bonus}
-              </button>
+              <div className={classes.categoryTitle}>{category[0].type}</div>
+              {category.map((ex: any) => (
+                <div
+                  className={classes.exampleWrapper}
+                  key={ex.idec || ex.bonus || ex.index}
+                >
+                  <button
+                    className={classes.playButton}
+                    onClick={() => {
+                      setId({
+                        ...DEFAULT_OPTIONS,
+                        ...(ex.idec ? { idec: ex.idec } : {}),
+                        ...(ex.bonus ? { bonus: ex.bonus } : {}),
+                        ...(ex.index ? { index: ex.index } : {}),
+                      });
+                    }}
+                  >
+                    <FaPlay color="#fafafa" />
+                  </button>
+                  <div className={classes.exampleDescription}>
+                    <div>{ex.title}</div>
+                    <div>
+                      <div className={classes.exampleId}>
+                        {`${ex.idec ? `idec ${ex.idec}` : ""}`}
+                        {`${ex.bonus ? `bonus ${ex.bonus}` : ""}`}
+                        {`${ex.index ? `index ${ex.index}` : ""}`}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+              <hr />
             </div>
           ))}
         </div>
